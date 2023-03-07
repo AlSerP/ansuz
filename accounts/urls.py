f11beb0c67faf7734297cd63edf205d029becac3
George <gosha1416@yandex.ru>
2023-03-07 15:45:42 +0300

    Fix

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('signup/', views.UserSignUpView.as_view(), name="signup"),
     path('group/create/', views.GroupCreateView.as_view(), name="create_group"),
     path('leaderboard/', views.LeadersView.as_view(), name="leaderboard"),
+    path('update_score/', views.update_all_users_score, name="update_all_score"),
     path('update_score/<int:pk>', views.update_user_score, name="update_score"),
-    path('update_score', views.update_user_score, name="update_score"),
 ]
 
